2357771c7669e03c156756ad8a5e99fce117ab79
lingceng <lingceng2008@gmail.com>
2016-01-16 18:21:14 +0800

    Add dict logic

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # BingDictionary
 
+这是一个ruby脚本，用来在命令行中直接查询单词含义，免去打开软件或浏览器的麻烦。
+
+Here is a command line dictionary written with ruby that can
+translate English to Chinese.
+
+It uses http://cn.bing.com/dict/ to query.
+![DEMO ](./dict.png)
+
 ## Installation
 
 Add this line to your application's Gemfile:
@@ -17,7 +25,51 @@ Or install it yourself as:
     $ gem install bing_dictionary
 
 ## Usage
+Download the dict file and make it runable.
+Current version need nokogiri installed:
 
+    gem install nokogiri
+
+DEMO：
+
+    ./dict cake
+    ./dict 蛋糕
+
+You'd better add configuration to $HOME/.bashrc or $HOME/.profile to make alias
+
+    alias d="/path/to/dict"
+
+You can make it work tegother with Vim by add follows to $HOME/.vimrc
+
+    nnoremap <leader>d :!dict <cword><CR>
+    vnoremap <leader>d y:!dict <c-r>"<CR>
+
+## Support long sentence
+Now long sentence is supported
+
+    ./dict 你好吗
+    ./dict How old are you
+
+## Support fuzzy query
+
+    ./dict hexxo
+
+    您要找的是不是
+
+    音近词
+    head coach总教练
+    hexose己醣
+    hexode六极管
+    ...
+
+## For Mac user
+You can open your local dictionary.app with `open dict://someword`. It may be faster.
+Add following to your vimrc to make it work in vim:
+
+    nnoremap <leader>dd :!open dic://<cword><CR>
+
+## Thanks
+Heavily borrowed from [Command-Line-Youdao-Dictionary](https://github.com/qhwa/Command-Line-Youdao-Dictionary)
 
 ## Development
 
--- lib/bing_dictionary.rb
+++ lib/bing_dictionary.rb
@@ -1,5 +1,86 @@
+require 'nokogiri'
+require 'open-uri'
+require 'cgi'
 require "bing_dictionary/version"
 
 module BingDictionary
-  # Your code goes here...
+  class Base
+    attr_reader :doc
+
+    def self.translate(word)
+      self.new(word).translate
+    rescue SocketError
+      warn 'Connection failed! Please check your network.'
+      exit 1
+    end
+
+    def initialize(word)
+      file = open("http://cn.bing.com/dict/?q=#{CGI::escape(word)}")
+      @doc = Nokogiri::HTML(file)
+    end
+
+    def translate
+      head if doc.at_css('#headword')
+      machine if doc.at_css('.smt_hw')
+      sentence if doc.at_css('#sentenceSeg .se_li')
+      guess if doc.at_css('.dym_area')
+    end
+
+    def head
+      puts doc.at_css('#headword').text
+      puts doc.at_css('.hd_tf_lh').text.green
+      puts
+      doc.at_css('.hd_area').next_sibling.css('li').each do |li|
+        puts li.at_css('.pos').text.fixed(5).blue + li.at_css('.def').text
+      end
+    end
+
+    def machine
+      puts doc.at_css('.smt_hw').text
+      puts doc.at_css('.p1-10').text
+      puts doc.at_css('.p1-11').text.green
+    end
+
+    def sentence
+      puts
+      doc.css('#sentenceSeg .se_li').first(4).map do |li|
+        puts li.css('.sen_en').text
+        puts li.css('.sen_cn').text
+        puts
+      end
+    end
+
+    def guess
+      puts
+      content = doc.at_css('.content')
+      puts content.at_css('.p2-2').text
+      puts
+
+      content.css('.dym_area').each do |area|
+          puts area.at_css('.df_wb_a').text
+          puts area.css('.df_wb_c').map(&:text)
+          puts
+      end
+    end
+
+  end
+
+end
+
+class String
+  COLORS = %w(black red green yellow blue magenta cyan white)
+  COLORS.each_with_index do |color, idx|
+    define_method color do
+      "\e[3#{idx}m" << self.to_s << "\e[0m"
+    end
+
+    define_method "#{color}_bg" do
+      "\e[4#{idx}m" << self.to_s << "\e[0m"
+    end
+  end
+
+  def fixed(width)
+    width = width - self.each_char.count { |c| c =~ /\p{Han}/ }
+    width > 0 ? ("%-#{width}s" % self) : self
+  end
 end
